4bb802666d80cea6a1825bd510e438a13ed81773
daoway <stas>
2025-10-16 02:06:56 +0300

    init

--- recursion-tree/SimpleIdent.java
+++ recursion-tree/SimpleIdent.java
@@ -1,16 +1,20 @@
 public class SimpleIdent {
     static int depth = 0;
 
-    public static int fib0(int n) throws Exception {
+    public static int fib0(int n) {
         // print with indentation
         String indent = "  ".repeat(depth);
         System.out.println(indent + "fib0(" + n + ") depth=" + depth);
 
         depth++;
         int result;
-        if (n == 0) result = 0;
-        else if (n == 1) result = 1;
-        else result = fib0(n - 1) + fib0(n - 2);
+        if (n == 0) {
+            result = 0;
+        } else if (n == 1) {
+            result = 1;
+        } else {
+            result = fib0(n - 1) + fib0(n - 2);
+        }
         depth--;
 
         System.out.println(indent + "=> fib0(" + n + ") = " + result);
