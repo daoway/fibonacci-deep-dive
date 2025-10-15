c962b352c4156a0e8e13441679352ca13b7a3b85
daoway <stas>
2025-10-16 00:02:31 +0300

    init

--- recursion-tree/RecursionGraphvizTree.java
+++ recursion-tree/RecursionGraphvizTree.java
@@ -6,22 +6,19 @@ import guru.nidi.graphviz.model.MutableNode;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static guru.nidi.graphviz.model.Factory.mutGraph;
 import static guru.nidi.graphviz.model.Factory.mutNode;
 
 public class RecursionGraphvizTree {
 
-    private static final AtomicInteger idCounter = new AtomicInteger(0);
+    private static int idCounter = 0;
 
     public static int fib(int n, MutableGraph graph, MutableNode parent) {
-        int myId = idCounter.getAndIncrement();
+        int myId = idCounter++;
 
-        String nodeName = "fib" + myId;
-        Label nodeLabel = Label.of("fib(" + n + ")");
-
-        MutableNode myNode = mutNode(nodeName).add(nodeLabel);
+        MutableNode myNode =
+                mutNode("fib" + myId).add(Label.of("fib(" + n + ")"));
         graph.add(myNode);
 
         if (parent != null) {
@@ -43,13 +40,15 @@ public class RecursionGraphvizTree {
     }
 
     public static void main(String[] args) throws IOException {
-        MutableGraph graph = mutGraph("Fibonacci").setDirected(true);
-        fib(5, graph, null);
-        Graphviz.fromGraph(graph)
-                .render(Format.DOT)
-                .toFile(new File("fib_tree.dot"));
-        Graphviz.fromGraph(graph)
+        MutableGraph g = mutGraph("Fibonacci").setDirected(true);
+        fib(5, g, null);
+
+        Graphviz.fromGraph(g)
                 .render(Format.PNG)
                 .toFile(new File("fib_tree.png"));
+
+        Graphviz.fromGraph(g)
+                .render(Format.DOT)
+                .toFile(new File("fib_tree.dot"));
     }
 }
