a1a77d2c45cad0287dd2050f89073661b4132915
daoway <stas>
2025-10-15 23:10:59 +0300

    init

--- recursion-tree/RecursionDotTree.java
+++ recursion-tree/RecursionDotTree.java
@@ -1,43 +1,52 @@
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class RecursionDotTree {
 
-    static class NodeId {
-        int id;
-        NodeId(int id) { this.id = id; }
-    }
-
     static int idCounter = 0;
 
-    public static int fib0(int n, FileWriter fw, NodeId parent)
-            throws IOException {
+    public static int fib0(int n, StringBuilder sb, NodeId parent) {
         int myId = idCounter++;
-        fw.write(String.format("  node%d [label=\"fib(%d)\"];\n", myId, n));
+        sb.append("node%d [label=\"fib(%d)\"];%n".formatted(myId, n));
 
         if (parent != null) {
-            fw.write(String.format("  node%d -> node%d;\n", parent.id, myId));
+            sb.append("  node%d -> node%d;%n".formatted(parent.id, myId));
         }
 
         int result;
-        if (n == 0) result = 0;
-        else if (n == 1) result = 1;
-        else {
-            int left = fib0(n - 1, fw, new NodeId(myId));
-            int right = fib0(n - 2, fw, new NodeId(myId));
+        if (n == 0) {
+            result = 0;
+        } else if (n == 1) {
+            result = 1;
+        } else {
+            int left = fib0(n - 1, sb, new NodeId(myId));
+            int right = fib0(n - 2, sb, new NodeId(myId));
             result = left + right;
         }
 
         return result;
     }
 
-    public static void main(String[] args){
-        try (FileWriter fw = new FileWriter("fib_tree.dot")) {
-            fw.write("digraph G {\n");
-            fib0(5, fw, null);
-            fw.write("}\n");
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        fib0(5, sb, null);
+        try {
+            Files.write(Paths.get("fib_tree.dot"), """
+                    digraph G {
+                        %s
+                    }%n
+                    """.formatted(sb.toString()).getBytes());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    static class NodeId {
+        int id;
+
+        NodeId(int id) {
+            this.id = id;
         }
     }
 }
