77482bc6aa323dc61d525b09b7619b72c89e7a0e
daoway <stas>
2025-10-16 16:59:19 +0300

    init

---
+++ data-types-limitations/BigIntegerFibonacci.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+
+public class BigIntegerFibonacci {
+    public static BigInteger fibBigDecimal(int n) {
+        if (n <= 0) {
+            return BigInteger.ZERO;
+        }
+        if (n == 1) {
+            return BigInteger.ONE;
+        }
+
+        BigInteger prev = BigInteger.ZERO;
+        BigInteger curr = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            BigInteger next = prev.add(curr);
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
+}
