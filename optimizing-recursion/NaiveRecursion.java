74150950f45c54bae2e19ae7665a191b9ad2c5ce
daoway <stas>
2025-10-16 19:46:55 +0300

    init

--- optimizing-recursion/NaiveRecursion.java
+++ optimizing-recursion/NaiveRecursion.java
@@ -1,10 +1,10 @@
-public class NaiveRecursion implements FibonacciNumber<Integer> {
-    public Integer fib(int n) {
+public class NaiveRecursion implements FibonacciNumber<Long> {
+    public Long fib(int n) {
         if (n == 0) {
-            return 0;
+            return 0L;
         }
         if (n == 1) {
-            return 1;
+            return 1L;
         }
         return fib(n - 1) + fib(n - 2);
     }
---
+++ recursive-time-exec/MeasureExecTime.java
@@ -0,0 +1,32 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MeasureExecTime {
+    private static final String CSV_HEADER = "n,Fn,time_sec";
+
+    public static void main(String[] args) {
+        FibonacciNumber<Long> naiveFibonacci = new NaiveRecursion();
+        measure(naiveFibonacci, "fibonacci_data.csv", 50);
+    }
+
+    private static void measure(FibonacciNumber<Long> algorithmImpl,
+                                String filename, int count) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filename))) {
+            writer.println(CSV_HEADER);
+            for (int n = 1; n <= count; n++) {
+                long startTime = System.nanoTime();
+                long result = algorithmImpl.fib(n);
+                long endTime = System.nanoTime();
+                double durationSec =
+                        (double) (endTime - startTime) / 1_000_000_000.0;
+                writer.printf("%d,%d,%.10f%n", n, result, durationSec);
+                System.out.printf("F(%d) calculated in %.4f sec.%n", n,
+                        durationSec);
+            }
+        } catch (IOException e) {
+            System.err.println(
+                    "Error while writing to file: " + e.getMessage());
+        }
+    }
+}
