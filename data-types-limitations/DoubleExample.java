604a858078a2eb551eff4176690845a95b721f90
daoway <stas>
2025-10-16 16:46:03 +0300

    init

---
+++ data-types-limitations/DoubleExample.java
@@ -0,0 +1,8 @@
+public class DoubleExample {
+    public static void main(String[] args) {
+        double x = 0.1;
+        double y = 0.2;
+        double z = x + y;
+        System.out.println(z);
+    }
+}
---
+++ data-types-limitations/DoubleExample.js
@@ -0,0 +1,4 @@
+const x = 0.1;
+const y = 0.2;
+const z = x + y;
+console.log(z);
