fa7a1d90beb7157736080ff1a96f5b13ff3d26bc
daoway <stas>
2025-10-19 02:06:06 +0300

    init

--- optimizing-recursion/FastPower.java
+++ optimizing-recursion/FastPower.java
@@ -1,27 +1,18 @@
 public class FastPower {
-    public static long pow(long x, long n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Exponent must be non-negative");
-        }
-        if (n == 0) {
-            return 1;
-        }
-
+    public static long pow(long base, long exponent) {
         long result = 1;
-        while (n > 0) {
-            if (n % 2 == 1) { // Якщо експонента непарна
-                result *= x;
+        while (exponent > 0) {
+            if (exponent % 2 == 1) {
+                result *= base;
             }
-            x *= x; // Зводимо базу в квадрат
-            n /= 2; // Ділимо експоненту на 2
+            base *= base;
+            exponent /= 2;
         }
-
         return result;
     }
 
     public static void main(String[] args) {
-        // Приклади використання
-        System.out.println(pow(2, 10)); // 1024
-        System.out.println(pow(3, 7));  // 2187
+        System.out.println(pow(3, 13));  // 1,594,323
+        //System.out.println(pow(3, 0));  // 1
     }
 }
