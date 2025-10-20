65c851d8ee41d9919bb41dfcffc79131156aa0bf
daoway <stas>
2025-10-20 19:44:56 +0300

    init

--- mclauren-series/not-efficient-fibonacci.py
+++ mclauren-series/not-efficient-fibonacci.py
@@ -1,18 +1,9 @@
 import sympy as sp
 
-# Символьна змінна
 x = sp.symbols('x')
-
-# Генеруюча функція Фібоначчі: F(x) = x / (1 - x - x^2)
 F = x / (1 - x - x**2)
-
-# 5-та похідна
 F5_deriv = sp.diff(F, x, 5)
-
-# Підставляємо x=0
 F5_at_0 = F5_deriv.subs(x, 0)
-
-# Ділимо на 5!
 F5_value = F5_at_0 / sp.factorial(5)
 
 print("F(x) =", F)
