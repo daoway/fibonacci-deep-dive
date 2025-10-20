286055072bb2a64dcf0af2a93a428cb001a0895a
daoway <stas>
2025-10-21 01:28:36 +0300

    init

---
+++ jvm-memory-structures/PassByValueExample.java
@@ -0,0 +1,26 @@
+public class PassByValueExample {
+    public static void main(String[] args) {
+        int number = 10;
+        StringBuilder sb = new StringBuilder("Hello");
+
+        System.out.println("Before method calls: number = " + number + ", sb = " + sb);
+
+        changeNumber(number);
+        modifyStringBuilder(sb);
+        reassignStringBuilder(sb);
+
+        System.out.println("After method calls: number = " + number + ", sb = " + sb);
+    }
+
+    public static void changeNumber(int x) {
+        x = 20; // Modifying the copy of the primitive value
+    }
+
+    public static void modifyStringBuilder(StringBuilder builder) {
+        builder.append(", World!"); // Modifying the object's state
+    }
+
+    public static void reassignStringBuilder(StringBuilder builder) {
+        builder = new StringBuilder("New String"); // Reassigning the reference
+    }
+}
