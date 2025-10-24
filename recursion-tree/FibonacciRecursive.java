d560b074bc40dfdcdc9684596fdf82de72ace954
daoway <stas>
2025-10-24 20:15:39 +0300

    init

--- recursion-tree/FibonacciRecursive.java
+++ recursion-tree/FibonacciRecursive.java
@@ -11,10 +11,11 @@ public class FibonacciRecursive {
         int callId = UUID.randomUUID().hashCode();
         listener.onCall(n, parentId, callId);
 
-        return switch (n) {
-            case 0 -> 0;
-            case 1 -> 1;
-            default -> fib(n - 1, callId) + fib(n - 2, callId);
-        };
+        if (n <= 1) {
+            return n;
+        } else {
+            //for next calls, current callId becomes parentId
+            return fib(n - 1, callId) + fib(n - 2, callId);
+        }
     }
 }
