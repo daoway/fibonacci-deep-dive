e11e59fd818c17414d81e67adc8f39947e557c62
daoway <stas>
2025-10-25 20:30:03 +0300

    init

---
+++ recursion-tree/GraphService.java
@@ -0,0 +1,96 @@
+import guru.nidi.graphviz.attribute.Color;
+import guru.nidi.graphviz.attribute.Label;
+import guru.nidi.graphviz.attribute.Shape;
+import guru.nidi.graphviz.attribute.Style;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.MutableGraph;
+import guru.nidi.graphviz.model.MutableNode;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static guru.nidi.graphviz.model.Factory.mutGraph;
+import static guru.nidi.graphviz.model.Factory.mutNode;
+
+public class GraphService {
+    private static final int IMAGE_WIDTH = 1200;
+    private static final int IMAGE_HEIGHT = 800;
+
+    private final MutableGraph graph;
+    private final Map<String, MutableNode> nodes = new HashMap<>();
+    private int stepCounter = 0;
+
+    public GraphService() {
+        this.graph = createGraph();
+    }
+
+    private MutableGraph createGraph() {
+        MutableGraph g = mutGraph("DFS").setDirected(true);
+        g.graphAttrs().add("viewport", "1200,800");
+        g.graphAttrs().add("dpi", "96");
+        g.nodeAttrs().add(Shape.CIRCLE);
+        g.nodeAttrs().add("fixedsize", "true");
+        g.nodeAttrs().add("width", "0.4");
+        g.nodeAttrs().add("height", "0.4");
+        g.nodeAttrs().add("fontsize", "12");
+        return g;
+    }
+
+    public void addNode(int value, String parentId, String callId) {
+        MutableNode node = createNode(value, callId);
+        graph.add(node);
+        nodes.put(callId, node);
+
+        if (parentId != null && nodes.containsKey(parentId)) {
+            nodes.get(parentId).addLink(node);
+        }
+    }
+
+    private MutableNode createNode(int value, String callId) {
+        return mutNode("node_" + callId)
+                .add(Label.of(String.valueOf(value)))
+                .add(Shape.CIRCLE)
+                .add(Style.FILLED)
+                .add(Color.WHITE.fill())
+                .add(Color.BLACK)
+                .add("fixedsize", "true")
+                .add("width", "0.4")
+                .add("height", "0.4")
+                .add("fontsize", "12");
+    }
+
+    public void highlight(String activeId) {
+        // Reset colors
+        for (MutableNode n : nodes.values()) {
+            n.add(Style.FILLED)
+                    .add(Color.WHITE.fill())
+                    .add(Color.BLACK);
+        }
+
+        // Highlight active node
+        MutableNode active = nodes.get(activeId);
+        if (active != null) {
+            active.add(Style.FILLED)
+                    .add(Color.rgb("ff6666").fill())
+                    .add(Color.BLACK);
+        }
+
+        saveStepImage();
+    }
+
+    private void saveStepImage() {
+        try {
+            stepCounter++;
+            Graphviz.fromGraph(graph)
+                    .width(IMAGE_WIDTH)
+                    .height(IMAGE_HEIGHT)
+                    .render(Format.PNG)
+                    .toFile(new File("step_%02d.png".formatted(stepCounter)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
